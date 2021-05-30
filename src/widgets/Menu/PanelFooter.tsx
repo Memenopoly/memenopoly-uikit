455933eb219525c440d7d956c58f7ad74acde651
famousbirthdays <>
2021-05-30 13:57:59 -0700

    remove theme selector and lang selector

--- src/widgets/Menu/PanelFooter.tsx
+++ src/widgets/Menu/PanelFooter.tsx
@@ -110,9 +110,9 @@ const PanelFooter: React.FC<Props> = ({
           })}
         </Flex>
       </SocialEntry>
-      <SettingsEntry>
+      { /* <SettingsEntry>
         <Button variant="text" onClick={() => toggleTheme(!isDark)}>
-          {/* alignItems center is a Safari fix */}
+          
           <Flex alignItems="center">
             <SunIcon color={isDark ? "textDisabled" : "text"} width="24px" />
             <Text color="textDisabled" mx="4px">
@@ -141,7 +141,7 @@ const PanelFooter: React.FC<Props> = ({
             </MenuButton>
           ))}
         </Dropdown>
-      </SettingsEntry>
+      </SettingsEntry> */ }
     </Container>
   );
 };
