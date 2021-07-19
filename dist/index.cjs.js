eacdd6303a90885e67713a1854cdb33144a420e4
famousbirthdays <>
2021-07-19 00:08:53 -0700

    change back card color

--- dist/index.cjs.js
+++ dist/index.cjs.js
@@ -2850,9 +2850,7 @@ var brandColors = {
 var lightColors = __assign(__assign(__assign({}, baseColors), brandColors), { background: "#e0eaea", backgroundDisabled: "#b3bbc3", contrast: "#191326", invertedContrast: "#FFFFFF", input: "#d3efff", tertiary: "#EFF4F5", text: "#142e44", textDisabled: "#BDC2C4", textSubtle: "#809dba", borderColor: "#c2c4c5", card: "#afafaf", gradients: {
         bubblegum: "linear-gradient(139.73deg, #E6FDFF 0%, #F3EFFF 100%)",
     } });
-var darkColors = __assign(__assign(__assign({}, baseColors), brandColors), { secondary: "#9A6AFF", background: "#343135", backgroundDisabled: "#3c3742", contrast: "#FFFFFF", invertedContrast: "#191326", input: "#483f5a", primaryDark: "#e01a1a", tertiary: "#353547", text: "#EAE2FC", textDisabled: "#666171", textSubtle: "#c9c4d4", borderColor: "#524B63", 
-    // card: "#27262c",
-    card: "#19171f", gradients: {
+var darkColors = __assign(__assign(__assign({}, baseColors), brandColors), { secondary: "#9A6AFF", background: "#343135", backgroundDisabled: "#3c3742", contrast: "#FFFFFF", invertedContrast: "#191326", input: "#483f5a", primaryDark: "#e01a1a", tertiary: "#353547", text: "#EAE2FC", textDisabled: "#666171", textSubtle: "#c9c4d4", borderColor: "#524B63", card: "#27262c", gradients: {
         bubblegum: "linear-gradient(139.73deg, #313D5C 0%, #3D2A54 100%)",
     } });
 
