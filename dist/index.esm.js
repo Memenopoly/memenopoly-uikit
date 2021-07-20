7be7ffa084c43217ef46de62776ba036ffddba22
famousbirthdays <>
2021-07-19 17:15:32 -0700

    more colors

--- dist/index.esm.js
+++ dist/index.esm.js
@@ -2808,8 +2808,8 @@ var baseColors = {
     primary: "#d42727",
     primaryBright: "#b50000",
     primaryDark: "#842121",
-    secondary: "#e8c50d",
-    success: "#809dba",
+    secondary: "#7645D9",
+    success: "#31D0AA",
     warning: "#FFB237",
 };
 var brandColors = {
@@ -2834,12 +2834,10 @@ var brandColors = {
   },
 };
 */
-var lightColors = __assign(__assign(__assign({}, baseColors), brandColors), { background: "#e0eaea", backgroundDisabled: "#b3bbc3", contrast: "#191326", invertedContrast: "#FFFFFF", input: "#d3efff", tertiary: "#47b150", text: "#142e44", textDisabled: "#BDC2C4", textSubtle: "#809dba", borderColor: "#c2c4c5", card: "#afafaf", gradients: {
+var lightColors = __assign(__assign(__assign({}, baseColors), brandColors), { background: "#e0eaea", backgroundDisabled: "#b3bbc3", contrast: "#191326", invertedContrast: "#FFFFFF", input: "#d3efff", tertiary: "#EFF4F5", text: "#142e44", textDisabled: "#BDC2C4", textSubtle: "#809dba", borderColor: "#c2c4c5", card: "#afafaf", gradients: {
         bubblegum: "linear-gradient(139.73deg, #E6FDFF 0%, #F3EFFF 100%)",
     } });
-var darkColors = __assign(__assign(__assign({}, baseColors), brandColors), { secondary: "#e8c50d", background: "#343135", backgroundDisabled: "#3c3742", contrast: "#FFFFFF", invertedContrast: "#191326", input: "#483f5a", primaryDark: "#235828", tertiary: "#47b150", text: "#EAE2FC", textDisabled: "#666171", textSubtle: "#c9c4d4", borderColor: "#524B63", 
-    // card: "#27262c",
-    card: "#19171f", gradients: {
+var darkColors = __assign(__assign(__assign({}, baseColors), brandColors), { secondary: "#9A6AFF", background: "#343135", backgroundDisabled: "#3c3742", contrast: "#FFFFFF", invertedContrast: "#191326", input: "#483f5a", primaryDark: "#0098A1", tertiary: "#353547", text: "#EAE2FC", textDisabled: "#666171", textSubtle: "#c9c4d4", borderColor: "#524B63", card: "#27262c", gradients: {
         bubblegum: "linear-gradient(139.73deg, #313D5C 0%, #3D2A54 100%)",
     } });
 
